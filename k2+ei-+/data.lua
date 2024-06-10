abf9177638734d3d42396ec14a2d4f077a70bbf2
Scrybatog <jernferns@gmail.com>
2024-06-10 19:37:39 -0400

    hi

--- k2+ei-+/data.lua
+++ k2+ei-+/data.lua
@@ -1,5 +1,15 @@
+ei_lib = require("lib/lib")
+
 local EI_CHANGES = {
     ["item"] = {
         ["ei_alien-seed"] = {localised_name = {"ei_alien-seed"},}
     },
 }
+
+for source, group in pairs(EI_CHANGES) do
+    for name, object in pairs(group) do
+        object.name = name
+        object.type = source
+        ei_lib.set_properties(object)
+    end
+end
---
+++ k2+ei-+/lib/lib.lua
@@ -0,0 +1,12 @@
+function ei_lib.set_properties(obj)
+    if not (obj and obj.name and obj.type) then
+        log(serpent.log({["Invalid object:"] = obj}))
+        return
+    end
+    local prototype = data.raw[obj.type][obj.name]
+    if not prototype then
+        log("Could not find prototype"..obj.type.."/"..obj.name)
+        return
+    end
+    recursive_copy(prototype, obj)
+end
